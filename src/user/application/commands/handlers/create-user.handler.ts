ba2b476c0c9b47e29b9373c0cbbaab97bca9e913
Lephat902 <lehoangngocphat992002@gmail.com>
2024-01-27 23:13:39 +0700

    Send confirmation email when creating user

--- src/user/application/commands/handlers/create-user.handler.ts
+++ src/user/application/commands/handlers/create-user.handler.ts
@@ -1,25 +1,57 @@
+import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { ICommandHandler, CommandHandler, EventPublisher } from '@nestjs/cqrs';
 import * as argon2 from 'argon2';
 import { CreateUserCommand } from '../impl/create-user.command';
-import { UserWriteRepository } from 'src/user/infrastructure/repositories';
+import { UserReadRepository, UserWriteRepository } from 'src/user/infrastructure/repositories';
 import { UserRepository } from 'src/user/domain/user.repository';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
+@Injectable()
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
     constructor(
-        private readonly _repository: UserWriteRepository,
+        private readonly writeRepository: UserWriteRepository,
+        private readonly readRepository: UserReadRepository,
         private readonly userRepository: UserRepository,
         private readonly _publisher: EventPublisher,
+        @Inject('MAIL_SERVICE') private readonly mailClient: ClientProxy,
+        @Inject('VERIFICATION_SERVICE') private readonly tokenClient: ClientProxy,
     ) { }
 
     async execute(command: CreateUserCommand) {
         const { createUserDto } = command;
+        const { email, username, password, ...otherFields } = createUserDto;
 
-        const { password, ...otherFields } = createUserDto;
+        // Check if email already exists
+        const existingEmailUser = await this.readRepository.findByEmail(email);
+        if (existingEmailUser) {
+            throw new BadRequestException('Email already exists');
+        }
+
+        // Check if username already exists
+        const existingUsernameUser = await this.readRepository.findByUsername(username);
+        if (existingUsernameUser) {
+            throw new BadRequestException('Username already exists');
+        }
 
         // Hash the provided password using argon2
         const hashedPassword = await argon2.hash(password);
-        const newUser = this._repository.create({ password: hashedPassword, ...otherFields });
+        let newUser = this.writeRepository.create({ password: hashedPassword, email, username, ...otherFields });
+
+        newUser = await this.writeRepository.save(newUser);
+
+        // Send confirmation token and email
+        const token = await firstValueFrom(
+            this.tokenClient.send<string>({ cmd: 'insert' }, newUser.id)
+        );
+
+        await firstValueFrom(
+            this.mailClient.send<string>({ cmd: 'sendUserConfirmation' }, {
+                user: { name: newUser.firstName, email: newUser.email },
+                token
+            })
+        );
 
         // Dispatch UserCreatedEvent event
         const user = this._publisher.mergeObjectContext(
@@ -28,6 +60,6 @@ export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
         user.commit();
 
         // Save the user to the repository
-        return await this._repository.save(newUser);
+        return newUser;
     }
 }
