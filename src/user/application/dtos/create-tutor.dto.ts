73dba25c755cbd314c2dc6922594c0df3513179c
Lephat902 <lehoangngocphat992002@gmail.com>
2024-03-12 10:52:40 +0700

    Change minimumWage type to number

--- src/user/application/dtos/create-tutor.dto.ts
+++ src/user/application/dtos/create-tutor.dto.ts
@@ -4,7 +4,7 @@ export class CreateTutorDto extends CreateBaseUserDto {
     readonly biography: string;
     readonly isApproved: boolean;
     readonly approvedAt: Date;
-    readonly minimumWage: string;
+    readonly minimumWage: number;
     readonly currentWorkplace: string;
     readonly currentPosition: string;
     readonly major: string;
